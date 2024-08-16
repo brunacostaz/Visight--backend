cd321cbcb43c9012da9dc46e4a01a86840a54f28
brunacostaz <brunacstcandeias@gmail.com>
2024-08-16 14:31:40 -0300

    pequenas modificações no print da mensagem

--- main.py
+++ main.py
@@ -9,19 +9,16 @@ import random
 
 tipo_alimento = forca_opcao(categoria_alimento,'Qual o tipo de alimento que você irá preparar? (carne/vegetal)\n-> ','Digite somente se o alimento é vegetal ou carne')
 alimento = input('Qual o alimento que você irá preparar? (ex: picanha, cenoura...)\n-> ')
-valor_alimento = verificar_num('Qual o valor pago por esse alimento?\n-> ')
 
 valor_min_kg = 1000
 valor_max_kg = 7000
 peso_bruto = random.randint(valor_min_kg,valor_max_kg)
 peso_liquido = random.randint(valor_min_kg,valor_max_kg)
+
 valor_min_g = 30
 valor_max_g = 300
 peso_corte = random.randint(valor_min_g,valor_max_g)
-print(f'Peso Bruto: {peso_bruto}\nPeso Liquído: {peso_liquido}\nPeso pós corte: {peso_corte}')
-
-peso_correto_l = 5000
-peso_correto_c = 70
+print(f'Peso Bruto: {peso_bruto} g\nPeso Liquído: {peso_liquido} g\nPeso pós corte: {peso_corte} g')
 
 rendimento = calcular_rendimento(peso_bruto,peso_liquido)
 preco_unidade = calcular_preco_unidade(peso_bruto,valor_alimento)
@@ -29,4 +26,4 @@ prejuizo = calcular_prejuizo(peso_bruto, peso_liquido, preco_unidade)
 desperdicio = calcular_desperdicio(peso_bruto,peso_liquido)
 percentual_desperdicio = desperdicio_percentual(peso_bruto,peso_liquido)
 
-print(f'\nAnálise de dados\n\nRendimento: {rendimento:.2f}%\nPrejuízo: {prejuizo:.2f}%\nDesperdício: {desperdicio:.2f} - equivalente a {percentual_desperdicio:.2f}%')
+print(f'\nAnálise de dados\n\nRendimento: {rendimento:.2f}%\nPrejuízo: {prejuizo:.2f}%\nDesperdício: {desperdicio:.2f} g - equivalente a {percentual_desperdicio:.2f}%')
