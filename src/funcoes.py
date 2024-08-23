1a76c8c92b0ce2dd0c9df6b257a409d3078499dd
Ricardo Cerazi Di Tilia <riiditilia@gmail.com>
2024-08-23 19:14:54 -0300

    arrumando funções de calculo de rendimento

--- src/funcoes.py
+++ src/funcoes.py
@@ -19,14 +19,28 @@ def verificar_num(msg):
     num = int(num)
     return num
 
-def calcular_rendimento(bruto,liquido):
+'''def calcular_rendimento(bruto,liquido):
     rendimento = (liquido/bruto)*100
     return rendimento
+
+
+def calcular_preco_unidade(bruto,valor):
+    preco_unidade = valor / bruto
+    return preco_unidade
+'''
+def calcular_prejuizo(peso_bruto,peso_liquido,preco_por_unidade):
+    custo_peso_bruto = peso_bruto * preco_por_unidade
+    custo_peso_liquido = peso_liquido * preco_por_unidade
+    prejuizo = custo_peso_bruto - custo_peso_liquido
+    return prejuizo
+
+
 
 def calcular_prejuizo(peso_bruto,peso_liquido,preco_por_kg):
     custo_peso_bruto = peso_bruto * preco_por_kg
     custo_peso_liquido = peso_liquido * preco_por_kg
-    prejuizo = custo_peso_bruto - custo_peso_liquido
+    qtd_desperdicio = custo_peso_bruto - custo_peso_liquido
+    prejuizo = qtd_desperdicio * preco_por_kg
     return prejuizo
 
 def calcular_desperdicio(bruto,liquido):
@@ -36,4 +50,5 @@ def calcular_desperdicio(bruto,liquido):
 def desperdicio_percentual(bruto,liquido):
     percentual_desperdicio = ((bruto - liquido) / bruto) * 100
     return percentual_desperdicio
+
 
