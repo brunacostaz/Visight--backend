f687f0d96e70c334897a85984c9e89195021eab4
brunacostaz <brunacstcandeias@gmail.com>
2024-08-23 16:50:57 -0300

    criação da lista de tipos de carne

--- src/listas.py
+++ src/listas.py
@@ -1,1 +1,4 @@
 categoria_alimento = ['vegetal', 'carne']
+tipos_carne = {'gado': ['picanha', 'file_mion', 'maminha', 'contrafile', 'alcatra', 'costela', 'fraldinha'], 'frango': 'frango', 'peixe': ['namorado', 'tilapia', 'salmao', 'porquinho', 'cacao', 'peixe_escada']}
+
+
