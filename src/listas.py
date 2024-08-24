e07edecd8f730a5f71e7b42daf3021fc3ede322c
brunacostaz <brunacstcandeias@gmail.com>
2024-08-24 18:23:19 -0300

    criação da lista com os meses

--- src/listas.py
+++ src/listas.py
@@ -1,4 +1,5 @@
 categoria_alimento = ['vegetal', 'carne']
+
 tipos_carne = {'gado': ['picanha', 'file_mion', 'maminha', 'contrafile', 'alcatra', 'costela', 'fraldinha'], 'frango': 'frango', 'peixe': ['namorado', 'tilapia', 'salmao', 'porquinho', 'cacao', 'peixe_escada']}
 
-
+meses = ['Janeiro 2024', 'Fevereiro 2024', 'Março 2024', 'Abril 2024', 'Maio 2024', 'Junho 2024', 'Julho 2024', 'Agosto 2024', 'Setembro 2024', 'Outubro 2024', 'Novembro 2024', 'Dezembro 2024']
