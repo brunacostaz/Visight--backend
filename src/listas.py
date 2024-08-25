d74c3a7ab52b284a8d3a1ec119f85eb64dc90b82
brunacostaz <brunacstcandeias@gmail.com>
2024-08-25 00:33:47 -0300

    acréscimo da lista dias_semana

--- src/listas.py
+++ src/listas.py
@@ -3,3 +3,5 @@ categoria_alimento = ['vegetal', 'carne']
 tipos_carne = {'gado': ['picanha', 'file_mion', 'maminha', 'contrafile', 'alcatra', 'costela', 'fraldinha'], 'frango': 'frango', 'peixe': ['namorado', 'tilapia', 'salmao', 'porquinho', 'cacao', 'peixe_escada']}
 
 meses = ['Janeiro 2024', 'Fevereiro 2024', 'Março 2024', 'Abril 2024', 'Maio 2024', 'Junho 2024', 'Julho 2024', 'Agosto 2024', 'Setembro 2024', 'Outubro 2024', 'Novembro 2024', 'Dezembro 2024']
+
+dias_semana = ['04/08/24', '05/08/24', '06/08/24', '07/08/24', '08/08/24', '09/08/24', '10/08/24']
