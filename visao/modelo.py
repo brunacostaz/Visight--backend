6f43ebf848a50b1ce7676e1bce5e4b8f031bc15f
brunacostaz <brunacstcandeias@gmail.com>
2024-09-20 21:36:29 -0300

    integração com a webcam (no momento nao funciona devido ao modelo que está dando erro)

--- visao/modelo.py
+++ visao/modelo.py
@@ -1,43 +1,74 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-from PIL import Image, ImageOps  # Install pillow instead of PIL
+# módulo do OpenCV usado para a captura de vídeo da webcam
+import cv2
+
+# função da biblioteca Keras (dentro do tensorflow) para carregar um modelo treinado carregado em um arquivo
+from tensorflow.keras.models import load_model
+
+# usado para abrir e manipular imagens - irá converter e redimensionar a imagem para o formato correto para o modelo
+from PIL import Image, ImageOps  
+
+# usado para transformar a imagem em uma estrutura de dados númericos (array, matrizes)
 import numpy as np
 
-# Disable scientific notation for clarity
+# desativa a notação científica ao exibir valores númericos
 np.set_printoptions(suppress=True)
 
-# Load the model
-model = load_model("keras_model.h5", compile=False)
+# carrega o modelo
+model = load_model("visao/keras_model.h5", compile=False)
 
-# Load the labels
+# carrega as classes que o modelo foi treinado para reconhecer
 class_names = open("classes.txt", "r").readlines()
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
+# np.ndarray() cria um array vazio, onde a imagem será armazenada para ser passada ao modelo
+# as especificações do shape() para o array são:
+# 1 -> significa que será processada uma imagem por vez
+# 224, 224 (pixels) -> refere-se ao tamanho da imagem que o modelo foi treinado para reconhecer
+# 3 -> refere-se aos canais de cor (RGB nesse caso)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Replace this with the path to your image
-image = Image.open("<IMAGE_PATH>").convert("RGB")
+# Inicializar a webcam
+cap = cv2.VideoCapture(0)
 
-# resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+# captura a imagem da webcam
+# ret é um booleano que indica se a captura foi bem-sucedida
+# frame é a imagem capturada, representada como um array numpy
+ret, frame = cap.read()
 
-# turn the image into a numpy array
-image_array = np.asarray(image)
+# Verificar se a captura foi bem-sucedida
+if ret:
+    # Converter o frame (imagem) em um objeto Image do PIL
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    # redimenciona a imagem para o tamanho esperado (224x224 pixels) e corta o excesso, se necessário
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-# Load the image into the array
-data[0] = normalized_image_array
+    # transforma a imagem em um array numérico 
+    image_array = np.asarray(image)
 
-# Predicts the model
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
+    # normaliza a imagem para um intervalo de -1 a 1, convertendo os valores dos pixels de 0 a 255 para -1 a 1, pois foi o que o modelo foi treinado
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-# Print prediction and confidence score
-print("Class:", class_name[2:], end="")
-print("Confidence Score:", confidence_score)
+    # carrega a imagem dentro do array criado anteriormente
+    data[0] = normalized_image_array
+
+    # passa a imagem processada (array) para o modelo e obtém as previsões
+    # o modelo retorna um array de probabilidades, onde cada posição corresponde à probabilidade de uma imagem pertencer a uma classe especifica  
+    prediction = model.predict(data)
+
+    # np.argmax() encontra o indice da classe com a maior probabilidade
+    index = np.argmax(prediction)
+
+    # armazena o nome da classe a partir do indice encontrado
+    class_name = class_names[index]
+
+    # obtem a probabilidade associada à classe prevista
+    confidence_score = prediction[0][index]
+
+    # exibe a classe com maior probabilidade
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", confidence_score)
+
+# Libera a câmera
+cap.release()
+
